da67f5b6f31defe9a5e2f2f9f05a4b77b86634da
anrybakov <wpandrewbox@gmail.com>
2020-04-01 15:53:59 -0400

    Lumen plugin stub

--- plugins/lumen/lumen.py
+++ plugins/lumen/lumen.py
@@ -9,42 +9,17 @@ from airflow.models.baseoperator import BaseOperator
 from airflow.plugins_manager import AirflowPlugin
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
 
-# # Will show up under airflow.hooks.test_plugin.PluginHook
-# class PluginHook(BaseHook):
-#     pass
-
-
-# # Will show up under airflow.operators.test_plugin.PluginOperator
-# class PluginOperator(BaseOperator):
-#     pass
-
-
-# # Will show up under airflow.sensors.test_plugin.PluginSensorOperator
-# class PluginSensorOperator(BaseSensorOperator):
-#     pass
-
-
-# # Will show up under airflow.executors.test_plugin.PluginExecutor
-# class PluginExecutor(BaseExecutor):
-#     pass
-
-
-# # Will show up under airflow.macros.test_plugin.plugin_macro
-# def plugin_macro():
-#     pass
-
-
 # Creating a flask appbuilder BaseView
-class PluginTestAppBuilderBaseView(AppBuilderBaseView):
+class LumenBuilderBaseView(AppBuilderBaseView):
 
     @expose("/")
     def list(self):
         return self.render_template("index.html", content="Hello galaxy!")
 
 
-v_appbuilder_view = PluginTestAppBuilderBaseView()
-v_appbuilder_package = {"name": "Test View",
-                        "category": "Test Plugin",
+v_appbuilder_view = LumenBuilderBaseView()
+v_appbuilder_package = {"name": "Lumen View",
+                        "category": "Lumen",
                         "view": v_appbuilder_view}
 
 # Creating a flask blueprint to intergrate the templates and static folder
@@ -52,29 +27,12 @@ bp = Blueprint(
     "lumen", __name__,
     template_folder='templates',  # registers airflow/plugins/templates as a Jinja template folder
     static_folder='static',
-    static_url_path='/static/lumen')
+    static_url_path='/lumen/static')
 
 
 # Defining the plugin class
 class AirflowTestPlugin(AirflowPlugin):
     name = "lumen"
-    # operators = [PluginOperator]
-    # sensors = [PluginSensorOperator]
-    # hooks = [PluginHook]
-    # executors = [PluginExecutor]
-    # macros = [plugin_macro]
     flask_blueprints = [bp]
     appbuilder_views = [v_appbuilder_package]
     # appbuilder_menu_items = []
-
-
-# class MockPluginA(AirflowPlugin):
-#     name = 'plugin-a'
-
-
-# class MockPluginB(AirflowPlugin):
-#     name = 'plugin-b'
-
-
-# class MockPluginC(AirflowPlugin):
-#     name = 'plugin-c'
