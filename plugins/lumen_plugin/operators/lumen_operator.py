ae4d51f65c16c4cbc3a4f81fa3c691063bfd1fb2
Courtney ericson <cericson@raybeam.com>
2020-04-16 13:25:06 -0500

    revamping sql alchemy call so it works

--- plugins/lumen_plugin/operators/lumen_operator.py
+++ plugins/lumen_plugin/operators/lumen_operator.py
@@ -1,6 +1,6 @@
 from airflow.operators import BaseOperator
 from airflow.utils.decorators import apply_defaults
-
+from airflow.models.taskinstance import TaskInstance
 from airflow.utils.db import create_session
 
 
@@ -21,17 +21,21 @@ class LumenOperator(BaseOperator):
     def __init__(
         self,
         test_name,
-        conn_id="kenshoo_default",
         *args,
         **kwargs
     ):
         super().__init__(*args, **kwargs)
         self.test_name = test_name
+        self.test_dag_id = test_name.split('.')[0]
+        self.test_task_id = test_name.split('.')[1]
 
     def execute(self, context):
         self.log.info("Querying postgres for %s's result.." % (self.test_name))
         # Query postgres and save to test_result
         with create_session() as curr_session:
-            output = curr_session.query(self.test_name).all()
+            output = curr_session.query(TaskInstance).filter(
+                TaskInstance.task_id == self.test_task_id,
+                TaskInstance.dag_id == self.test_dag_id,
+            ).order_by(TaskInstance.execution_date.desc()).first()
             self.log.info("\n\n\nQuery output:\n%s\n\n\n" % output)
         return True
