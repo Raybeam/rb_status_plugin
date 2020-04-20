3b29f56affacfcb154da78ed157a64f0cafe47f1
Courtney ericson <cericson@raybeam.com>
2020-04-20 12:16:35 -0500

    fixing remaining test compile errors

--- plugins/lumen_plugin/tests/lumen_sensor_test.py
+++ plugins/lumen_plugin/tests/lumen_sensor_test.py
@@ -1,51 +1,59 @@
 from plugins.lumen_plugin.sensors.lumen_sensor import LumenSensor
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.db import create_session
 from airflow.models.taskinstance import TaskInstance
-from datetime import datetime
+from datetime import datetime, timedelta
 import unittest
 from airflow.utils.state import State
 
+from airflow import DAG
+
+# Default settings applied to all tests
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "start_date": datetime(2020, 4, 19),
+    "retry_delay": timedelta(minutes=5),
+}
+
 class LumenSensorTest(unittest.TestCase):
-
-    def __add_to_task_instance(self, task, state):
-        ti = TaskInstance(
-            task=task,
-            execution_date=datetime(2020, 4, 20)
-        )
-        ti.set_state(state)
-        with create_session() as curr_session:
-            ti = curr_session.add(ti)
-
-    def __create_context(self, task_id):
-        with create_session() as curr_session:
-            ti = curr_session.query(TaskInstance).filter(
-                TaskInstance.task_id == self.task_id
-            ).order_by(TaskInstance.execution_date.desc()).first()
-
     def test_success(self):
         # test that LumenOperator correctly interprets successful test
         task_state = True
         expected_response = True
+        dag = DAG('adhoc_Airflow', schedule_interval=None, default_args=default_args)
+        dummy_success = DummyOperator(
+            task_id='dummy_success',
+            dag=dag
+        )
         sensor = LumenSensor(
             task_id="test_%s" % task_state,
-            test_name=f"lumen_operator_test.test_expected_to_{task_state}"
+            test_name=f"adhoc_Airflow.dummy_success"
         )
-        self.__add_to_task_instance(sensor, State.SUCCESS)
-        context = self.__create_context("test_%s" % task_state,).get_template_context()
-        result = LumenSensor.execute(context)
+        ti = TaskInstance(task=dummy_success, execution_date=datetime(2020, 4, 20))
+        context = ti.get_template_context()
+        result = sensor.poke(context=context)
         self.assertEqual(expected_response, result)
 
     def test_failure(self):
         # test that LumenOperator correctly interprets failed test
         task_state = False
         expected_response = False
+        dag = DAG('adhoc_Airflow', schedule_interval=None, default_args=default_args)
+        dummy_failure = DummyOperator(
+            task_id='dummy_failure',
+            dag=dag
+        )
         sensor = LumenSensor(
             task_id="test_%s" % task_state,
-            test_name=f"lumen_operator_test.test_expected_to_{task_state}"
+            test_name=f"adhoc_Airflow.dummy_failure"
         )
-        self.__add_to_task_instance(sensor, State.FAILED)
-        context = self.__create_context("test_%s" % task_state).get_template_context()
-        result = LumenSensor.execute(context)
+        ti = TaskInstance(task=dummy_failure, execution_date=datetime(2020, 4, 20))
+        context = ti.get_template_context()
+        result = sensor.poke(context=context)
         self.assertEqual(expected_response, result)
 
 
