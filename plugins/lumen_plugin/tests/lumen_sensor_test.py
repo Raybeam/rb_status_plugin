f15c8538a10f01d248eece1c95f7d13bfea2923c
Courtney ericson <cericson@raybeam.com>
2020-04-20 11:37:44 -0500

    fixing sql errors

--- plugins/lumen_plugin/tests/lumen_sensor_test.py
+++ plugins/lumen_plugin/tests/lumen_sensor_test.py
@@ -3,26 +3,23 @@ from airflow.utils.db import create_session
 from airflow.models.taskinstance import TaskInstance
 from datetime import datetime
 import unittest
-
+from airflow.utils.state import State
 
 class LumenSensorTest(unittest.TestCase):
-    def __run_lumen_operator(self, task_state, context):
-        # run Lumen operator against temporary dag and show results
-        return LumenSensor.execute(context)
 
-    def __add_to_task_instance(self, task):
+    def __add_to_task_instance(self, task, state):
+        ti = TaskInstance(
+            task=task,
+            execution_date=datetime(2020, 4, 20)
+        )
+        ti.set_state(state)
         with create_session() as curr_session:
-            ti = curr_session.add(
-                TaskInstance(
-                    task=task,
-                    execution_date=datetime(2020, 4, 20)
-                )
-            )
+            ti = curr_session.add(ti)
 
-    def __create_context(self):
+    def __create_context(self, task_id):
         with create_session() as curr_session:
             ti = curr_session.query(TaskInstance).filter(
-                TaskInstance.task_id == self.test_task_id
+                TaskInstance.task_id == self.task_id
             ).order_by(TaskInstance.execution_date.desc()).first()
 
     def test_success(self):
@@ -33,8 +30,8 @@ class LumenSensorTest(unittest.TestCase):
             task_id="test_%s" % task_state,
             test_name=f"lumen_operator_test.test_expected_to_{task_state}"
         )
-        self.__add_to_task_instance(sensor)
-        context = self.__create_context().get_template_context()
+        self.__add_to_task_instance(sensor, State.SUCCESS)
+        context = self.__create_context("test_%s" % task_state,).get_template_context()
         result = LumenSensor.execute(context)
         self.assertEqual(expected_response, result)
 
@@ -46,8 +43,8 @@ class LumenSensorTest(unittest.TestCase):
             task_id="test_%s" % task_state,
             test_name=f"lumen_operator_test.test_expected_to_{task_state}"
         )
-        self.__add_to_task_instance(sensor)
-        context = self.__create_context().get_template_context()
+        self.__add_to_task_instance(sensor, State.FAILED)
+        context = self.__create_context("test_%s" % task_state).get_template_context()
         result = LumenSensor.execute(context)
         self.assertEqual(expected_response, result)
 
