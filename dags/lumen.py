0e4e8c868dc1b59327f576e0ad8b4196729f549f
Courtney ericson <cericson@raybeam.com>
2020-04-23 13:42:18 -0500

    formatting all files changed with black

--- dags/lumen.py
+++ dags/lumen.py
@@ -22,7 +22,7 @@ default_args = {
 airflow_home = os.environ["AIRFLOW_HOME"]
 
 # Consider moving these constants to an Airflow variable...
-EMAIL_TEMPLATE_LOCATION = f'{airflow_home}/plugins/lumen_plugin/templates/emails'
+EMAIL_TEMPLATE_LOCATION = f"{airflow_home}/plugins/lumen_plugin/templates/emails"
 
 SINGLE_STATUS_EMAIL_TEMPLATE_LOC = f"{EMAIL_TEMPLATE_LOCATION}/single_report.html"
 MULTIPLE_STATUS_EMAIL_TEMPLATE_LOC = f"{EMAIL_TEMPLATE_LOCATION}/multiple_report.html"
@@ -30,35 +30,30 @@ MULTIPLE_STATUS_EMAIL_TEMPLATE_LOC = f"{EMAIL_TEMPLATE_LOCATION}/multiple_report
 
 def create_dag(report, default_args):
     dag = DAG(
-        report.dag_id,
-        schedule_interval=report.schedule,
-        default_args=default_args
+        report.dag_id, schedule_interval=report.schedule, default_args=default_args
     )
 
     with dag:
-        test_prefix = 'test_'
+        test_prefix = "test_"
 
         start = DummyOperator(task_id="start_dag")
-        send_report = DummyOperator(
-            task_id="send_report",
-            trigger_rule="all_done",
-        )
+        send_report = DummyOperator(task_id="send_report", trigger_rule="all_done",)
         send_email = PythonOperator(
-            task_id='call_email_function',
+            task_id="call_email_function",
             python_callable=report_notify_email,
             trigger_rule="all_done",
             op_kwargs={
                 "emails": report.emails,
                 "email_template_location": SINGLE_STATUS_EMAIL_TEMPLATE_LOC,
-                "test_prefix": test_prefix
+                "test_prefix": test_prefix,
             },
-            provide_context=True
+            provide_context=True,
         )
         for test in report.tests:
             t1 = LumenSensor(
                 task_id="%s%s" % (test_prefix, test),
-                test_dag_id=test.split('.')[0],
-                test_task_id=test.split('.')[1]
+                test_dag_id=test.split(".")[0],
+                test_task_id=test.split(".")[1],
             )
             start >> t1 >> [send_report, send_email]
 
--- plugins/lumen_plugin/helpers/email_helpers.py
+++ plugins/lumen_plugin/helpers/email_helpers.py
@@ -4,22 +4,25 @@ from airflow.utils.state import State
 from datetime import datetime
 import re
 
+
 def are_all_tasks_successful(test_prefix, context):
-    '''
+    """
     Uses the context dictionary passed via the Python Operator
     to iterate over all the test tasks and return True if all pass
     and False if otherwise
-    '''
+    """
     dag_instance = context["dag"]
     execution_date = context["execution_date"]
     tasks = dag_instance.task_ids
 
-    test_regex = f'\b{test_prefix}'
+    test_regex = f"\b{test_prefix}"
     for task in tasks:
         # Evaluate only test tasks via regex
         if re.match(test_regex, task):
             operator_instance = dag_instance.get_task(task)
-            task_status = TaskInstance(operator_instance, execution_date).current_state()
+            task_status = TaskInstance(
+                operator_instance, execution_date
+            ).current_state()
             if task_status == State.FAILED:
                 return False
     return True
@@ -37,7 +40,7 @@ def report_notify_email(emails, email_template_location, test_prefix, **context)
     :type test_prefix: str
     """
     report_passed = are_all_tasks_successful(test_prefix, context)
-    dag_name = context['ti'].dag_id
+    dag_name = context["ti"].dag_id
     email_subject = f"[{report_passed}] {report_name}"
 
     with open(email_template_location) as file:
@@ -45,16 +48,15 @@ def report_notify_email(emails, email_template_location, test_prefix, **context)
             task_id="custom_email_notification",
             to=emails,
             subject=email_subject,
-            html_content=file.read()
+            html_content=file.read(),
         )
         params = {
             "passed": report_passed,
             "updated": datetime.now(),
             "title": dag_name,
-            "details_link": "#"
+            "details_link": "#",
         }
         send_email.render_template_fields(
-            context=params,
-            jinja_env=context['dag'].get_template_env()
+            context=params, jinja_env=context["dag"].get_template_env()
         )
         send_email.execute(context)
