83575bb14ab8090249434a2f7c90d544e1c0df4e
Courtney ericson <cericson@raybeam.com>
2020-04-23 16:39:35 -0500

    running black on files

--- plugins/lumen_plugin/helpers/email_helpers.py
+++ plugins/lumen_plugin/helpers/email_helpers.py
@@ -4,6 +4,7 @@ from plugins.lumen_plugin.report_instance import ReportInstance
 from airflow.utils.state import State
 from datetime import datetime
 import re
+import logging
 
 
 def create_report_instance(context):
@@ -18,6 +19,15 @@ def get_test_status(report_instance):
     return report_instance.errors()
 
 
+def get_error_details(test_prefix, errors):
+    """
+    This just returns the first error log link
+    """
+    for failed_task in errors:
+        if re.match(test_prefix, failed_task["name"]) is not None:
+            return failed_task["description"]
+
+
 def are_all_tasks_successful(test_prefix, errors):
     """
     Iterate over all the test tasks status and return True if all pass
@@ -29,18 +39,13 @@ def are_all_tasks_successful(test_prefix, errors):
 
     for failed_task in errors:
         # If the failed task is a test task
-        if re.match(test_prefix, task) is not None:
+        if re.match(test_prefix, failed_task["name"]) is not None:
             return False
 
     return True
 
 
-def report_notify_email(
-    emails,
-    email_template_location,
-    test_prefix,
-    **context
-):
+def report_notify_email(emails, email_template_location, test_prefix, **context):
     """
     :param emails: emails to send report status to
     :type emails: list
@@ -53,23 +58,27 @@ def report_notify_email(
     """
     ri = create_report_instance(context)
 
-    errors = get_test_status(ri)
-    report_passed = are_all_tasks_successful(test_prefix, errors)
-    status = "Success" if report_passed else "Failed"
-
     dag_name = ri.id
     updated_time = ri.updated
-    email_subject = f"[{status}] {dag_name}"
+    errors = get_test_status(ri)
+    details_link = get_error_details(test_prefix, errors)
+    passed = are_all_tasks_successful(test_prefix, errors)
+
+    if passed:
+        status = "Success"
+    else:
+        status = "Failed"
 
     with open(email_template_location) as file:
         send_email = EmailOperator(
             task_id="custom_email_notification",
             to=emails,
-            subject=email_subject,
+            subject="[{{status}}] {{title}}",
             html_content=file.read(),
         )
         params = {
-            "passed": report_passed,
+            "passed": passed,
+            "status": status,
             "updated": updated_time,
             "title": dag_name,
             "details_link": "#",
@@ -77,4 +86,5 @@ def report_notify_email(
         send_email.render_template_fields(
             context=params, jinja_env=context["dag"].get_template_env()
         )
+        logging.info(f'Sending "{send_email.subject}" email...')
         send_email.execute(context)
