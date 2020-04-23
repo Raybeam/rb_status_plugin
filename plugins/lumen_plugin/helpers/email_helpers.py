6396043b84717a4a3d57368c98128ee4fdec6644
Courtney ericson <cericson@raybeam.com>
2020-04-23 13:34:20 -0500

    adding regex to only consider tasks matching regex where task begins with test_

--- plugins/lumen_plugin/helpers/email_helpers.py
+++ plugins/lumen_plugin/helpers/email_helpers.py
@@ -2,28 +2,31 @@ from airflow.operators.email_operator import EmailOperator
 from airflow.models.taskinstance import TaskInstance
 from airflow.utils.state import State
 from datetime import datetime
-
+import re
 
 def are_all_tasks_successful(context):
+    '''
+    Uses the context dictionary passed via the Python Operator
+    to iterate over all the test tasks and return True if all pass
+    and False if otherwise
+    '''
     dag_instance = context["dag"]
     execution_date = context["execution_date"]
     tasks = dag_instance.task_ids
     for task in tasks:
-        operator_instance = dag_instance.get_task(task)
-        task_status = TaskInstance(operator_instance, execution_date).current_state()
-        if task_status == State.FAILED:
-            return False
+        # Evaluate only test tasks via regex
+        if re.match('\btest_', task):
+            operator_instance = dag_instance.get_task(task)
+            task_status = TaskInstance(operator_instance, execution_date).current_state()
+            if task_status == State.FAILED:
+                return False
     return True
-
-
-def get_dag_id_from_context(context):
-    return context['ti'].dag_id
 
 
 def report_notify_email(email_template_location, emails, **context):
     """Send custom email alerts."""
     report_passed = are_all_tasks_successful(context)
-    title = get_dag_id_from_context(context)
+    dag_name = context['ti'].dag_id
     email_subject = f"[{report_passed}] {report_name}"
 
     with open(email_template_location) as file:
@@ -34,10 +37,10 @@ def report_notify_email(email_template_location, emails, **context):
             html_content=file.read()
         )
         params = {
-                "passed": report_passed,
-                "updated": datetime.now(),
-                "title": title,
-                "details_link": "#"
+            "passed": report_passed,
+            "updated": datetime.now(),
+            "title": dag_name,
+            "details_link": "#"
         }
         send_email.render_template_fields(
             context=params,
