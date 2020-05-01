bc4a756575ac65e6dcb634687139f32663ed682f
Courtney ericson <cericson@raybeam.com>
2020-05-01 13:54:27 -0500

    Fixing caching of report instance var

--- plugins/lumen_plugin/report_instance.py
+++ plugins/lumen_plugin/report_instance.py
@@ -24,7 +24,7 @@ class ReportInstance:
     @property
     def passed(self):
         if self._passed is None:
-            self.calculatePassed(self.errors())
+            self._passed = self.calculatePassed(self.errors())
         return self._passed
 
     @property
