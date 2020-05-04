4c0013bf2a9f6b73bde2e2d1b80b550ea3e983d0
Courtney ericson <cericson@raybeam.com>
2020-05-04 14:54:48 -0500

    fixing flake8 formatting

--- plugins/lumen_plugin/helpers/list_tasks_helper.py
+++ plugins/lumen_plugin/helpers/list_tasks_helper.py
@@ -2,7 +2,6 @@ from airflow.utils.db import provide_session
 from airflow.models.taskinstance import TaskInstance
 from plugins.lumen_plugin.report_repo import VariablesReportRepo
 from airflow.utils.state import State
-import logging
 
 @provide_session
 def get_all_test_choices(session=None):
@@ -11,5 +10,5 @@ def get_all_test_choices(session=None):
         ~TI.dag_id.like(f"{VariablesReportRepo.report_prefix}%"),
         TI.state != State.REMOVED
     ).all()
-    test_choices = [ {"id": i, "name": ti.task_id} for (i, ti) in enumerate(tis)]
+    test_choices = [{"id": i, "name": ti.task_id} for (i, ti) in enumerate(tis)]
     return test_choices
