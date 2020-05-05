c581ef9e2ecea20ac9d246308163b83f5ce96eac
mtsadler <msadler@raybeam.com>
2020-05-05 14:54:02 -0500

    updated report_repo_test's dummy_test to use schedule_type=custom

--- plugins/lumen_plugin/tests/report_repo_test.py
+++ plugins/lumen_plugin/tests/report_repo_test.py
@@ -23,8 +23,8 @@ class VariablesReportRepoTest(unittest.TestCase):
                     "example_dag.python_print_date_0",
                     "example_dag.python_random_0"
                 ],
+                "schedule_type": "custom",
                 "schedule": "* * * 1 *",
-                "schedule_type": ""
             }
             """
 
