a41723ccfe26a260b9ef4cff26a8629ab0ecc31d
anrybakov <wpandrewbox@gmail.com>
2020-05-05 16:03:41 -0400

    url_prefix needed to serve static files

--- plugins/lumen_plugin/__init__.py
+++ plugins/lumen_plugin/__init__.py
@@ -45,7 +45,8 @@ bp = Blueprint(
     __name__,
     template_folder="templates",
     static_folder="static",
-    static_url_path="/static",
+    # static_url_path="/static",
+    url_prefix="/lumen",
 )
 
 
