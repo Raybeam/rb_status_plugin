a6d71e43cc27dd29d4d9e0f0f61e6b284e4b5374
Courtney ericson <cericson@raybeam.com>
2020-05-05 13:12:45 -0500

    we really should have lumen_ as a variable

--- plugins/lumen_plugin/helpers/list_tasks_helper.py
+++ plugins/lumen_plugin/helpers/list_tasks_helper.py
@@ -2,14 +2,17 @@ from airflow.utils.db import provide_session
 from airflow.models.taskinstance import TaskInstance
 from plugins.lumen_plugin.report_repo import VariablesReportRepo
 from airflow.utils.state import State
+import logging
 
 
 @provide_session
 def get_all_test_choices(session=None):
     TI = TaskInstance
     tis = session.query(TI).filter(
-        ~TI.dag_id.like(f"{VariablesReportRepo.report_prefix}%"),
+        ~TI.dag_id.like(f"lumen_%"),
         TI.state != State.REMOVED
     ).all()
+    logging.info(tis)
+    logging.info(f"{VariablesReportRepo.report_prefix}%")
     test_choices = [(i, ti.task_id) for (i, ti) in enumerate(tis)]
     return test_choices
