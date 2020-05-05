a232c0fa413385247c92a374b1a9c1f6a877c418
Courtney ericson <cericson@raybeam.com>
2020-05-05 17:21:48 -0500

    fixing import

--- plugins/lumen_plugin/helpers/trigger_dag_helpers.py
+++ plugins/lumen_plugin/helpers/trigger_dag_helpers.py
@@ -1,10 +1,10 @@
 from airflow.utils.db import provide_session
-from airflow.models.dag import DAG, DAGModel
+from airflow.models.dag import DagModel
 
 
 @provide_session
 def trigger_dag_id(dag_id, session=None):
-    dag = session.query(DAGModel).filter(
-        DAGModel.dag_id == dag_id
+    dag = session.query(DagModel).filter(
+        DagModel.dag_id == dag_id
     ).first()
     dag.run()
