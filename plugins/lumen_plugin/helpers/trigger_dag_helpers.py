abc667f88ff35bb04f7f5aa05d5c4a2f0870cd96
Courtney ericson <cericson@raybeam.com>
2020-05-05 17:15:11 -0500

    dag model is the correct class not dag

--- plugins/lumen_plugin/helpers/trigger_dag_helpers.py
+++ plugins/lumen_plugin/helpers/trigger_dag_helpers.py
@@ -1,10 +1,10 @@
 from airflow.utils.db import provide_session
-from airflow.models.dag import DAG
+from airflow.models.dag import DAG, DAGModel
 
 
 @provide_session
 def trigger_dag_id(dag_id, session=None):
-    dag = session.query(DAG).filter(
-        DAG.dag_id == dag_id
+    dag = session.query(DAGModel).filter(
+        DAGModel.dag_id == dag_id
     ).first()
     dag.run()
