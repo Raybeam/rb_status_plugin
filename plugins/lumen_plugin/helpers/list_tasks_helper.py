98660eabc311c020aaa4fb4551a1f479f76ac080
Courtney ericson <cericson@raybeam.com>
2020-05-06 12:48:36 -0500

    removing changes

--- plugins/lumen_plugin/helpers/list_tasks_helper.py
+++ plugins/lumen_plugin/helpers/list_tasks_helper.py
@@ -1,7 +1,7 @@
 from airflow.utils.db import provide_session
 from airflow.models.taskinstance import TaskInstance
 from airflow.utils.state import State
-import logging
+
 
 @provide_session
 def get_all_test_choices(session=None):
@@ -10,7 +10,5 @@ def get_all_test_choices(session=None):
         ~TI.dag_id.like(f"lumen_%"),
         TI.state != State.REMOVED
     ).with_entities(TI.dag_id, TI.task_id).distinct().all()
-    test_name = f"{ti.dag_id}.{ti.task_id}"
-    test_choices = [(test_name, test_name) for (i, ti) in enumerate(tis)]
-    logging.info(test_choices)
+    test_choices = [(f"{ti.dag_id}.{ti.task_id}", f"{ti.dag_id}.{ti.task_id}") for (i, ti) in enumerate(tis)]
     return test_choices
