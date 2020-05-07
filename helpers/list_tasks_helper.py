b692d45ac96cea69db1b1f997f5f3aa8cb92c02c
mtsadler <msadler@raybeam.com>
2020-05-07 14:39:31 -0500

    updated filter and sorted tests

--- helpers/list_tasks_helper.py
+++ helpers/list_tasks_helper.py
@@ -7,9 +7,10 @@ from airflow.utils.state import State
 def get_all_test_choices(session=None):
     TI = TaskInstance
     tis = session.query(TI).filter(
-        ~TI.dag_id.like(f"lumen\_%"),
+        ~TI.dag_id.like(r"lumen\_%".format()),
         TI.state != State.REMOVED
     ).with_entities(TI.dag_id, TI.task_id).distinct().all()
     test_choices = [(f"{ti.dag_id}.{ti.task_id}", f"{ti.dag_id}.{ti.task_id}")
                     for (i, ti) in enumerate(tis)]
+    test_choices.sort()
     return test_choices
