dec552a45529019e0770945e5d7760177808af2a
anrybakov <wpandrewbox@gmail.com>
2020-05-18 09:23:35 -0400

    remove NewReportFormViewAdmin

--- views_no_rbac.py
+++ views_no_rbac.py
@@ -3,14 +3,12 @@ from flask_admin import BaseView, expose
 from lumen_plugin.views import (
     LumenStatusView,
     LumenReportsView,
-    NewReportFormView,
 )
 from lumen_plugin.report_repo import VariablesReportRepo
 
 
 lumen_status_view_rbac = LumenStatusView()
 lumen_reports_view_rbac = LumenReportsView()
-lumen_new_report_form_view_rbac = NewReportFormView()
 
 class LumenStatusViewAdmin(BaseView):
     @expose('/')
@@ -24,12 +22,3 @@ class LumenReportsViewAdmin(BaseView):
     @expose('/')
     def test(self):
         return self.render('no_rbac/reports.html', context=VariablesReportRepo.list())
-
-class NewReportFormViewAdmin(BaseView):
-    @expose("/", methods=["GET"])
-    def this_form_get(self):
-        return lumen_new_report_form_view_rbac.this_form_get()
-
-    @expose("/", methods=["POST"])
-    def form_post(self):
-        return lumen_new_report_form_view_rbac.this.form_post()
