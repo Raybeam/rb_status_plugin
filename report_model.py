f75028fdda0a5258e58d3f9d981a8e2370b2764f
ceyevar <courtney.ericson@yahoo.com>
2020-05-20 13:39:31 -0500

    adding functions we need

--- report_model.py
+++ report_model.py
@@ -1,4 +1,45 @@
 from flask_admin.model import BaseModelView
 
 class ReportModel(BaseModelView):
-  pass
+  #The BaseModelView class's __init__ takes in a model which is then defined as self.model
+  
+  # Implementing the required BaseModelView functions
+  def get_pk_value(self):
+    return None
+
+  def scaffold_list_columns(self):
+    return None
+
+  def scaffold_sortable_columns(self):
+    return None
+
+  def init_search(self):
+    return None
+
+  def scaffold_form(self):
+    class MyForm(Form):
+      pass
+    # Do something
+    return MyForm
+
+  def get_list(self):
+    return None
+  
+  def get_one(self):
+    return None
+  
+  def create_model(self):
+    return None
+  
+  def update_model(self):
+    return None
+  
+  def delete_model(self)
+    return None
+  
+  def is_valid_filter(self):
+    return None
+  
+  def scaffold_filters(self):
+    return None
+
