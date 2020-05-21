ede26f6935959fce7afb1c09d1011f65b93b7fcd
Courtney ericson <cericson@raybeam.com>
2020-05-21 15:34:26 -0500

    changing report title

--- report_model.py
+++ report_model.py
@@ -142,7 +142,7 @@ class ReportModel(BaseModelView):
     def scaffold_form(self):
         class ReportForm(Form):
             report_id = HiddenField()
-            title = StringField(
+            report_title = StringField(
                 ("Title"),
                 description="Title will be used as the report's name",
                 # widget=BS3TextFieldWidget(),
