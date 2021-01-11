df57ca8017ddeba59f81063b69f517ad89c20018
root <root@WL0990-0049032.bbby.corp.int>
2021-01-11 16:44:08 -0600

    resolved schedule_timezone missing for non-rbac, added option to use custom email server

--- core/helpers/email_helpers.py
+++ core/helpers/email_helpers.py
@@ -1,8 +1,12 @@
 from airflow.operators.email_operator import EmailOperator
 from airflow.configuration import conf
+from airflow.models import Variable
 
 import logging
 import pendulum
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from rb_status_plugin.core.report_instance import ReportInstance
 from rb_status_plugin.core.views import StatusView
@@ -53,6 +57,7 @@ def report_notify_email(report, email_template_location, **context):
     passed = ri.passed
     status = get_status(passed)
     details_link = get_details_link()
+    use_sendgrid = True if Variable.get("use_sendgrid", "false").lower() == 'true' else False
 
     with open(email_template_location) as file:
         send_email = EmailOperator(
@@ -71,5 +76,30 @@ def report_notify_email(report, email_template_location, **context):
         send_email.render_template_fields(
             context=params, jinja_env=context["dag"].get_template_env()
         )
-        logging.info(f'Sending "{send_email.subject}" email...')
-        send_email.execute(context)
+
+        if use_sendgrid:
+            logging.info(f'Sending "{send_email.subject}" email...')
+            send_email.execute(context)
+        else:
+            subject = send_email.subject
+            message = send_email.html_content
+            dag_id = context["dag"].dag_id
+            composer_instance_name = conf.get('webserver', 'web_server_name')
+            smtp_server = Variable.get("email_server", "smtp.bedbath.com")
+            sender_email = Variable.get(
+                f"{ dag_id }_sender_email",
+                f"no-reply@{ composer_instance_name }.bedbath.com",
+            )
+            receivers = ", ".join(report.subscribers)
+            msg = MIMEMultipart("alternative")
+            msg["Subject"] = subject
+            msg["From"] = sender_email
+            msg["To"] = receivers
+            mime_message = MIMEText(message, "html")
+            msg.attach(mime_message)
+
+            logging.info("Establishing connection with SMTP server..")
+            server = smtplib.SMTP(f"{ smtp_server }:25")
+            server.sendmail(sender_email, report.subscribers, msg.as_string())
+            server.quit()
+
